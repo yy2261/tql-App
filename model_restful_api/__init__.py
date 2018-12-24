d42b00a0f1e5004d5b24d724babd18de0daee0e2
尐 <313303303@qq.com>
2018-12-24 12:33:33 +0800

    Create __init__.py

---
+++ model_restful_api/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_restful.reqparse import RequestParser
+
+
+def restful_api(model=object):
+    class Restful(Resource, Model):
+
+        def __init__(self):
+            super().__init__()
+
+        def get(self):
+            request = self.parser.parse_args(strict=True)
+            return self.predict(**request)
+
+        @classmethod
+        def app(cls):
+            app = Flask(__name__)
+            api = Api(app)
+            api.add_resource(cls, cls().routing)
+            app.run(debug=True)
+
+    return Restful
