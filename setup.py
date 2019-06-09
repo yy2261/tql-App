1f9e0882d7f72f1aac75932d903bf0ca9a476d9e
尐 <313303303@qq.com>
2019-06-09 14:14:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,14 +40,14 @@ setup(
     include_package_data=True,
     package_data={'': ['*.*']},
     platforms=["all"],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: Implementation',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries'
     ],
 
